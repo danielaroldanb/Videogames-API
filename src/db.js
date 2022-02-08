ef700bc0894f09f4469e1b9314aacb3cb9f62722
danielaroldanb <89650193+danielaroldanb@users.noreply.github.com>
2022-02-08 18:20:55 -0500

    Deploy

--- src/db.js
+++ src/db.js
@@ -2,14 +2,17 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
+const { DB } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+sequelize = new Sequelize(DB, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+}
+);
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
